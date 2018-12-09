286a266fea737b1d390504a0bc72144ff5fc7b46
Christian Welzel <gawain@camlann.de>
2018-12-09 11:59:49 +0100

    Use icon factory.
    # Conflicts:
    #	Classes/Backend/TCA/TceFuncSelectTreeView.php

--- Classes/Backend/TCA/TceFuncSelectTreeView.php
+++ Classes/Backend/TCA/TceFuncSelectTreeView.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace TYPO3\CMS\Cal\Backend\TCA;
-
-use TYPO3\CMS\Backend\Tree\View\AbstractTreeView;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * This file is part of the TYPO3 extension Calendar Base (cal).
@@ -21,80 +20,72 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 /**
  * This function displays a selector with nested categories.
  * The original code is borrowed from the extension "Digital Asset Management" (tx_dam) author: Rene© Fritz <r.fritz@colorcube.de>
+ *
+ * @author Mario Matzulla <mario(at)matzullas.de>
+ * @package TYPO3
+ * @subpackage cal
  */
 
 /**
  * extend class \TYPO3\CMS\Backend\Tree\View\AbstractTreeView to change function wrapTitle().
  */
-class TceFuncSelectTreeView extends AbstractTreeView
-{
-    public $TCEforms_itemFormElName = '';
-    public $TCEforms_nonSelectableItemsArray = [];
+class TceFuncSelectTreeView extends \TYPO3\CMS\Backend\Tree\View\AbstractTreeView {
+	var $TCEforms_itemFormElName = '';
+	var $TCEforms_nonSelectableItemsArray = array ();
 
-    /**
-     * TceFuncSelectTreeView constructor.
-     *
-     * @deprecated since ext:cal version 2.x. Will be removed in version 3.0.0
-     */
-    public function __construct()
-    {
-        $this->init();
-    }
+	public function __construct() {
+		parent::__construct();
 
-    /**
-     * wraps the record titles in the tree with links or not depending on if they are in the TCEforms_nonSelectableItemsArray.
-     *
-     * @param string $title :
-     *            title
-     * @param array $v :
-     *            array with uid and title of the current item.
-     * @return string wrapped title
-     * @deprecated since ext:cal version 2.x. Will be removed in version 3.0.0
-     */
-    public function wrapTitle($title, $v, $bank = 0)
-    {
-        if ($v['uid'] > 0) {
-            if (in_array($v['uid'], $this->MOUNTS) || in_array($v['uid'], $this->TCEforms_nonSelectableItemsArray)) {
-                return '<a href="#" title="' . $v['title'] . '"><span style="color:#999;cursor:default;">' . $title . '</span></a>';
-            }
-            $aOnClick = 'setFormValueFromBrowseWin(\'' . $this->TCEforms_itemFormElName . '\',' . $v['uid'] . ',\'' . addslashes($title) . '\'); return false;';
-            return '<a href="#" onclick="' . htmlspecialchars($aOnClick) . '" title="' . htmlentities($v['title']) . '">' . $title . '</a>';
-        }
-        return $title;
-    }
+       $this->init();
+	}
 
-    /**
-     * Get icon for the row.
-     * If $this->iconPath and $this->iconName is set, try to get icon based on those values.
-     *
-     * @param
-     *            array        Item row.
-     * @return string tag.
-     * @deprecated since ext:cal version 2.x. Will be removed in version 3.0.0
-     */
-    public function getIcon($row)
-    {
-        if (in_array($row['uid'], $this->MOUNTS)) {
-            $this->table = 'sys_category';
-        }
-        $return = parent::getIcon($row);
-        $this->table = 'sys_category';
-        return $return;
-    }
+	/**
+	 * wraps the record titles in the tree with links or not depending on if they are in the TCEforms_nonSelectableItemsArray.
+	 *
+	 * @param $title    string title
+	 * @param $v        array  array with uid and title of the current item.
+	 * @param $bank     unused
+	 * @return string wrapped title
+	 */
+	public function wrapTitle($title, $v, $bank = 0) {
+		if ($v ['uid'] > 0) {
+			if (in_array ($v ['uid'], $this->MOUNTS) || in_array ($v ['uid'], $this->TCEforms_nonSelectableItemsArray)) {
+				return '<a href="#" title="' . $v ['title'] . '"><span style="color:#999;cursor:default;">' . $title . '</span></a>';
+			} else {
+				$aOnClick = 'setFormValueFromBrowseWin(\'' . $this->TCEforms_itemFormElName . '\',' . $v ['uid'] . ',\'' . addslashes ($title) . '\'); return false;';
+				return '<a href="#" onclick="' . htmlspecialchars ($aOnClick) . '" title="' . htmlentities ($v ['title']) . '">' . $title . '</a>';
+			}
+		} else {
+			return $title;
+		}
+	}
 
-    /**
-     * Returns the root icon for a tree/mountpoint (defaults to the globe)
-     *
-     * @param
-     *            array        Record for root.
-     * @return string image tag.
-     * @deprecated since ext:cal version 2.x. Will be removed in version 3.0.0
-     */
-    public function getRootIcon($rec)
-    {
-        return $this->wrapIcon(
-            '<img src="' . ExtensionManagementUtility::extPath('cal') . 'res/icons/icon_tx_cal_calendar.gif" width="18" height="16" alt="" />',
-            []
-        );
-    }
+	/**
+	 * Get icon for the row.
+	 * If $this->iconPath and $this->iconName is set, try to get icon based on those values.
+	 *
+	 * @param  array		Item row.
+	 * @return string tag.
+	 */
+	public function getIcon($row) {
+		if (in_array ($row ['uid'], $this->MOUNTS)) {
+			$this->table = 'tx_cal_calendar';
+		}
+		$return = parent::getIcon ($row);
+		$this->table = 'tx_cal_category';
+		return $return;
+	}
+
+	/**
+	 * Returns the root icon for a tree/mountpoint (defaults to the globe)
+	 *
+	 * @param  array		Record for root.
+	 * @return string image tag.
+	 */
+	public function getRootIcon($rec) {
+		$iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+		return $iconFactory->getIcon('cal-pagetree-root', Icon::SIZE_SMALL)->render();
+	}
 }
+
+?>
