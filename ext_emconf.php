b357b127b7333f534f35c5aa8175073592bf0ea4
tudy <email@thomasruta.de>
2019-03-11 11:48:27 +0100

    [fix] flexform.xml [fix] with php-cs-fix to php 7.y

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,40 +1,33 @@
 <?php
 
-/**
- * *************************************************************
- * Extension Manager/Repository config file for ext "cal".
- *
- * Auto generated 01-05-2013 10:23
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- * *************************************************************
+/*
+ * This file is part of the web-tp3/cal.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
-$EM_CONF [$_EXTKEY] = array (
-		'title' => 'Calendar Base',
-		'description' => 'A calendar combining all the functions of the existing calendar extensions plus adding some new features. It is based on the ical standard',
-		'category' => 'plugin',
-		'shy' => 0,
-		'version' => '1.11.2-dev',
-		'loadOrder' => '',
-		'state' => 'stable',
-		'uploadfolder' => 0,
-		'createDirs' => 'uploads/tx_cal/pics,uploads/tx_cal/ics,uploads/tx_cal/media',
-		'clearCacheOnLoad' => 0,
-		'author' => 'Mario Matzulla, Jeff Segars, Franz Koch, Thomas Kowtsch',
-		'author_email' => 'mario@matzullas.de, jeff@webempoweredchurch.org, franz.koch@elements-net.de, typo3@thomas-kowtsch.de',
-		'author_company' => '',
-		'constraints' => array (
-				'depends' => array (
-						'typo3' => '6.1.0-8.9.99' 
-				),
-				'suggests' => array (
-						'css_styled_content' => '6.1.0-8.9.99',
-						'wec_map' => '',
-						'tt_address' => ''
-				)
-		)
-);
 
-?>
+$EM_CONF [$_EXTKEY] = [
+        'title' => 'Calendar Base',
+        'description' => 'A calendar combining all the functions of the existing calendar extensions plus adding some new features. It is based on the ical standard',
+        'category' => 'plugin',
+        'shy' => 0,
+        'version' => '1.11.2-dev',
+        'loadOrder' => '',
+        'state' => 'stable',
+        'uploadfolder' => 0,
+        'createDirs' => 'uploads/tx_cal/pics,uploads/tx_cal/ics,uploads/tx_cal/media',
+        'clearCacheOnLoad' => 0,
+        'author' => 'Mario Matzulla, Jeff Segars, Franz Koch, Thomas Kowtsch',
+        'author_email' => 'mario@matzullas.de, jeff@webempoweredchurch.org, franz.koch@elements-net.de, typo3@thomas-kowtsch.de',
+        'author_company' => '',
+        'constraints' => [
+                'depends' => [
+                        'typo3' => '6.1.0-8.9.99'
+                ],
+                'suggests' => [
+                        'css_styled_content' => '6.1.0-8.9.99',
+                        'wec_map' => '',
+                        'tt_address' => ''
+                ]
+        ]
+];
